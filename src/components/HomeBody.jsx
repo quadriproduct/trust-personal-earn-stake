d50c958fc8e36b82d4f47d0c5c77022fccb8b6c9
Chilo Nwokolo <chilo.nwokolo@gmail.com>
2022-05-22 17:12:45 +0100

    update name and images

--- src/components/HomeBody.jsx
+++ src/components/HomeBody.jsx
@@ -1,5 +1,5 @@
-// import Hero from "../assets/images/hero.png";
-import Hero from "../assets/images/ethereum-logo-home.png";
+import Hero from "../assets/images/hero.png";
+// import Hero from "../assets/images/ethereum-logo-home.png";
 import { Link } from "react-router-dom";
 
 export default function () {
@@ -7,11 +7,11 @@ export default function () {
     <div className="container col-xxl-8 px-4 py-5">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
-        <img src={Hero} className="d-block mx-lg-auto img-fluid hero_img" alt="trust logo" width="900" height="800" loading="lazy" />
+        <img src={Hero} className="d-block mx-lg-auto img-fluid hero_img" alt="trust logo" width="800" height="800" loading="lazy" />
       </div>
       <div className="col-lg-6">
         <h1 className="display-5 fw-bold lh-1 mb-3">Welcome to Trust!</h1>
-        <p className="fs-5">Where Web2 and Web3 users strive</p>
+        <p className="fs-5">Real-time Investing and Payment for Businesses and Employees</p>
         <p className="fs-5">Sign up and join the new world of finance</p>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start mt-5">
           <Link to="/business-registration">
